3f033c7e33634dbc73f0fe43dd31a69793749dda
Wes Williams <wes.williamz@gmail.com>
2011-06-16 09:45:13 -0500

    added chart url file

--- wes_test/lib/wes_test.rb
+++ wes_test/lib/wes_test.rb
@@ -16,31 +16,34 @@ class WesTest
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript">
       MingleJavascript.register(function initialize() {
-        var drawChart = function() {
-          var data = new google.visualization.DataTable();
-          data.addColumn('string', 'Day');
-          data.addColumn('number', 'Ideal');
-          data.addRow(["6/1/11", 6]);
-          data.addRow(["6/2/11", 5]);
-          data.addRow(["6/3/11", 4]);
-          data.addRow(["6/4/11", 3]);
-          data.addRow(["6/5/11", 2]);
-          data.addRow(["6/6/11", 1]);
-          data.addRow(["6/7/11", 0]);
-          // Create and draw the visualization.
-          //new google.visualization.LineChart(document.getElementById('burndown_chart')).
-          //  draw(data, {curveType: "function",
-          //          width: 600, height: 400,
-          //          vAxis: {maxValue: 8}});
-        };
-        google.load("visualization", "1", {packages:["corechart"]});
-        // google.setOnLoadCallback(drawChart);
-        // drawChart();
+       alert('Test macro javascript');
       });
     </script>
     HTML
   end
 
+#  $('burndown_chart').innerHTML = 'burn down div';
+#  var drawChart = function() {
+#    $('burndown_chart').innerHTML = 'burn down div';
+#    //var data = new google.visualization.DataTable();
+#    //data.addColumn('string', 'Day');
+#    //data.addColumn('number', 'Ideal');
+#    //data.addRow(["6/1/11", 6]);
+#    //data.addRow(["6/2/11", 5]);
+#    //data.addRow(["6/3/11", 4]);
+#    //data.addRow(["6/4/11", 3]);
+#    //data.addRow(["6/5/11", 2]);
+#    //data.addRow(["6/6/11", 1]);
+#    //data.addRow(["6/7/11", 0]);
+#    // Create and draw the visualization.
+#    //new google.visualization.LineChart(document.getElementById('burndown_chart')).
+#    //  draw(data, {curveType: "function",
+#    //          width: 600, height: 400,
+#    //          vAxis: {maxValue: 8}});
+#  };
+#  google.load('visualization', '1', {'packages':['corechart']}
+#  google.setOnLoadCallback(drawChart);
+#  drawChart();
 
 
   def can_be_cached?
