31e5640ab9b55aba9726bb2a7a48caeceaeb130d
Wes Williams <wes.williamz@gmail.com>
2011-06-17 11:58:55 -0500

    query for story point and accepted on

--- wes_test/lib/wes_test.rb
+++ wes_test/lib/wes_test.rb
@@ -14,30 +14,32 @@ class WesTest
     @project = project
     @current_user = current_user
   end
-    
+
   def execute
 #    @parameters.each {|key, value| html << "param: '#{key} = #{value}'<br>"}
-    
+
     date_range = iteration_date_range
-    weekdays = get_weekdays(date_range)
+    weekdays = weekdays_for(date_range)
 
     <<-HTML
     h2. Iteration ##{@parameters['current_iteration']} Burndown:
 
     weekdays = #{weekdays}
+    story info #{story_info.to_s}
 
     <img src='https://chart.googleapis.com/chart?cht=lxy&chs=600x400&chds=a&chtt=Iteration%20Burndown&chls=1,6,6&chxt=x,y&chxr=1,0,11,1&chma=50,0,0,50&chdl=Ideal%20Line|Burndown&chco=00FF00,FF0000&chd=t:0,1,2,3,4|11,8.25,5.5,2.75,0|0,1,2,3,4|11,11,6,3,0&chxl=0:|#{weekdays}|1:||1|2|3|4|'></img>
     HTML
   end
 
-  def get_weekdays(date_range)
-    weekdays = ((date_range.begin)..(date_range.end)).select {|day| WEEKDAYS.include? day.wday }
-    weekdays.collect {|day| "#{day.month}-#{day.day}"}.join('|')
-  end
-
-  def iteration
-    iteration = @parameters['current_iteration']
-    iteration ||= /#(\d+).*/.match(@project.value_of_project_variable('Current Iteration'))[1].to_i
+  def story_info
+    begin
+      iteration = /#\d+ (.*)/.match(current_iteration)[1]
+      data_rows = @project.execute_mql(
+          "SELECT 'Planning Estimate', 'Accepted On' WHERE type is Story AND Iteration = '#{iteration}'")
+      data_rows.each {|hash| hash.update(hash) {|key, value| (key == 'accepted_on' && value) ? Date.parse(value) : value } }
+    rescue Exception
+      "[error retrieving story info for iteration '#{iteration}': #{$!}]"
+    end
   end
 
   def iteration_date_range
@@ -50,41 +52,21 @@ class WesTest
     end
   end
 
-#  <div id="burndown_chart" style="width: 600px; height: 400px"></div>
-#  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  def weekdays_for(date_range)
+    weekdays = ((date_range.begin)..(date_range.end)).select { |day| WEEKDAYS.include? day.wday }
+    weekdays.collect { |day| "#{day.month}-#{day.day}" }.join('|')
+  end
 
-#  <script type="text/javascript">
-#    MingleJavascript.register(function initialize() {
-#     alert('Test macro javascript');
-#    });
-#  </script>
+  def current_iteration
+    @project.value_of_project_variable('Current Iteration')
+  end
 
-#  $('burndown_chart').innerHTML = 'burn down div';
-#  var drawChart = function() {
-#    $('burndown_chart').innerHTML = 'burn down div';
-#    //var data = new google.visualization.DataTable();
-#    //data.addColumn('string', 'Day');
-#    //data.addColumn('number', 'Ideal');
-#    //data.addRow(["6/1/11", 6]);
-#    //data.addRow(["6/2/11", 5]);
-#    //data.addRow(["6/3/11", 4]);
-#    //data.addRow(["6/4/11", 3]);
-#    //data.addRow(["6/5/11", 2]);
-#    //data.addRow(["6/6/11", 1]);
-#    //data.addRow(["6/7/11", 0]);
-#    // Create and draw the visualization.
-#    //new google.visualization.LineChart(document.getElementById('burndown_chart')).
-#    //  draw(data, {curveType: "function",
-#    //          width: 600, height: 400,
-#    //          vAxis: {maxValue: 8}});
-#  };
-#  google.load('visualization', '1', {'packages':['corechart']}
-#  google.setOnLoadCallback(drawChart);
-#  drawChart();
-
+  def iteration
+    /#(\d+).*/.match(current_iteration)[1].to_i
+  end
 
   def can_be_cached?
-    false  # if appropriate, switch to true once you move your macro to production
+    false # if appropriate, switch to true once you move your macro to production
   end
 
 end
