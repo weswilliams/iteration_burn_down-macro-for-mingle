2620b173bbcd1ec20373254cf0ab539c46e04ec6
Wes Williams <wes.williamz@gmail.com>
2011-06-17 14:38:56 -0500

    idea line fully built with iteration data

--- wes_test/lib/wes_test.rb
+++ wes_test/lib/wes_test.rb
@@ -17,30 +17,46 @@ class WesTest
 
   def execute
 #    @parameters.each {|key, value| html << "param: '#{key} = #{value}'<br>"}
+    begin
+      chart_url = 'https://chart.googleapis.com/chart?'
+      chart_title = 'chtt=Iteration%20Burndown'
+      date_range = iteration_date_range
+      weekdays = weekdays_for(date_range)
+      stories = story_info
+      total_story_points = calculate_total_story_points stories
+      chart_range = "chxr=1,0,#{total_story_points},1"
+      ideal_line_data = generate_idea_line_data(total_story_points, date_range)
+#      ideal_line_data = "#{total_story_points},4.5,3,1.5,0"
+      burn_down_line = "6,5,4,3,0"
 
-    chart_url = 'https://chart.googleapis.com/chart?'
-    chart_title = 'chtt=Iteration%20Burndown'
-    date_range = iteration_date_range
-    weekdays = weekdays_for(date_range)
-    stories = story_info
-    total_story_points = calculate_total_story_points stories
-    chart_range = "chxr=1,0,#{total_story_points},1"
-    ideal_line_data = "#{total_story_points},4.5,3,1.5,0"
-    burn_down_line = "6,5,4,3,0"
-
-    <<-HTML
+      <<-HTML
     h2. Iteration ##{@parameters['current_iteration']} Burndown:
 
     weekdays = #{weekdays} <br>
     total story points #{total_story_points} <br>
+    date range #{date_range} <br>
+    idea line data = #{ideal_line_data} <br>
     story info #{story_info.to_s}
 
     <img src='#{chart_url}cht=lxy&chs=600x400&chds=a&#{chart_title}&chls=1,6,6&chxt=x,y&#{chart_range}&chma=50,0,0,50&chdl=Ideal%20Line|Burndown&chco=00FF00,FF0000&chd=t:0,1,2,3,4|#{ideal_line_data}|0,1,2,3,4|#{burn_down_line}&chxl=0:|#{weekdays}|1:||1|2|3|4|'></img>
-    HTML
+      HTML
+    rescue Exception
+      "Something went way wrong: #{$!}"
+    end
+  end
+
+  def generate_idea_line_data(total_story_points, date_range)
+#    "6,4.5,3,1.5,0"
+    number_of_weekdays = ((date_range.begin)..(date_range.end)).select { |day| WEEKDAYS.include? day.wday }.count
+    step = (total_story_points*1.0) / (number_of_weekdays-1)
+    idea_data = []
+    (0.0..total_story_points).step(step) { |value| idea_data << value }
+    idea_data
+    idea_data.reverse.join(',')
   end
 
   def calculate_total_story_points(stories)
-    stories.inject(0) {|total, hash| hash['planning_estimate'] ? total + hash['planning_estimate'].to_i : total}
+    stories.inject(0) { |total, hash| hash['planning_estimate'] ? total + hash['planning_estimate'].to_i : total }
   end
 
   def story_info
@@ -48,7 +64,7 @@ class WesTest
       iteration = /#\d+ (.*)/.match(current_iteration)[1]
       data_rows = @project.execute_mql(
           "SELECT 'Planning Estimate', 'Accepted On' WHERE type is Story AND Iteration = '#{iteration}'")
-      data_rows.each {|hash| hash.update(hash) {|key, value| (key == 'accepted_on' && value) ? Date.parse(value) : value } }
+      data_rows.each { |hash| hash.update(hash) { |key, value| (key == 'accepted_on' && value) ? Date.parse(value) : value } }
     rescue Exception
       "[error retrieving story info for iteration '#{iteration}': #{$!}]"
     end
