01e42bc9b4df8cfc0a4e1ed37d533d55e9df2a7d
Wes Williams <wes.williamz@gmail.com>
2011-06-18 07:04:05 -0500

    - added spec for iteration date range query

--- iteration_burn_down/test/spec/data_query_spec.rb
+++ iteration_burn_down/test/spec/data_query_spec.rb
@@ -1,5 +1,26 @@
 require "rspec"
+require "date"
 require "../../lib/iteration_burn_down_macro"
+
+describe "iteration date range query" do
+  before do
+    @iteration_date_range = [{'start_date' => '2011-06-01', 'end_date' => '2011-06-07'}]
+    @parameters = {}
+    @project = double('project',
+                      :value_of_project_variable => '#3 Iteration 1',
+                      :execute_mql => @iteration_date_range)
+    @burn_down_macro = IterationBurnDownMacro.new(@parameters, @project, nil)
+  end
+
+  subject { @burn_down_macro.iteration_date_range }
+  context "query with default parameters" do
+    before do
+      @project.should_receive(:execute_mql).with(
+          "SELECT 'Start Date', 'End Date' WHERE Number = 3")
+    end
+    it { should == ((Date.parse('2011-06-01'))..(Date.parse('2011-06-07'))) }
+  end
+end
 
 describe "burn down query" do
   before do
@@ -8,21 +29,17 @@ describe "burn down query" do
     ]
     @parameters = {}
     @project = double('project',
-      :value_of_project_variable => '#3 Iteration 1',
-      :execute_mql => @story_info)
+                      :value_of_project_variable => '#3 Iteration 1',
+                      :execute_mql => @story_info)
     @burn_down_macro = IterationBurnDownMacro.new(@parameters, @project, nil)
   end
 
-  subject { @burn_down_macro }
-
-  describe "story estimate and accepted on query" do
-    subject { @burn_down_macro.story_info }
-    context "query with default parameters" do
-      before do
-        @project.should_receive(:execute_mql).with(
+  subject { @burn_down_macro.story_info }
+  context "query with default parameters" do
+    before do
+      @project.should_receive(:execute_mql).with(
           "SELECT 'Story Points', 'Date Accepted' WHERE type is Story AND Iteration = 'Iteration 1'")
-      end
-      it { should == @story_info }
     end
+    it { should == @story_info }
   end
 end
