d1752ffccd6b254a931cb2a907583783ec153cd6
Wes Williams <wes.williamz@gmail.com>
2011-06-18 07:26:50 -0500

    - added spec for limiting the y axis to 10 major grid points

---
+++ iteration_burn_down/test/spec/y_axis_step_spec.rb
@@ -0,0 +1,36 @@
+require "rspec"
+require "../../lib/iteration_burn_down_macro"
+
+describe "steps between y grid values" do
+  describe "limit y axis to 10 major points in grid" do
+    before do
+      @burn_down_macro = IterationBurnDownMacro.new({}, nil, nil)
+    end
+
+    context "when total story points is 10" do
+      subject { @burn_down_macro.y_axis_step 10 }
+      it { should == 1 }
+    end
+
+    context "when total story points is < 10" do
+      subject { @burn_down_macro.y_axis_step 5 }
+      it { should == 1 }
+    end
+
+    context "when total story points is > 10" do
+      subject { @burn_down_macro.y_axis_step 11 }
+      it { should == 2 }
+    end
+
+    context "when total story points is < 21" do
+      subject { @burn_down_macro.y_axis_step 20 }
+      it { should == 2 }
+    end
+
+    context "when total story points is > 20" do
+      subject { @burn_down_macro.y_axis_step 21 }
+      it { should == 3 }
+    end
+
+  end
+end
