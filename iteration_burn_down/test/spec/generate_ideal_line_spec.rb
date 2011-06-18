44206cd695685d1c4ed61ea89cc29cbb4daa7a68
Wes Williams <wes.williamz@gmail.com>
2011-06-18 07:13:59 -0500

    - added spec for ideal line data

---
+++ iteration_burn_down/test/spec/generate_ideal_line_spec.rb
@@ -0,0 +1,14 @@
+require "rspec"
+require "../../lib/iteration_burn_down_macro"
+
+describe "generate ideal line data" do
+  before do
+    @burn_down_macro = IterationBurnDownMacro.new({}, nil, nil)
+  end
+
+  subject { @burn_down_macro.generate_idea_line_data 4, (Date.parse('2011-06-01')..(Date.parse('2011-06-07'))) }
+  context "ideal points completed each day excluding weekends" do
+    it { should == "4.0,3.0,2.0,1.0,0.0" }
+  end
+
+end
