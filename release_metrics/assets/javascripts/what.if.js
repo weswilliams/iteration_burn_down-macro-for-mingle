d3f6bc787b63451f983e4eb8d2d1c48cd990774a
Wes Williams <wes.williamz@gmail.com>
2011-07-14 13:03:47 -0500

    - moved javascript to .js file

---
+++ release_metrics/assets/javascripts/what.if.js
@@ -0,0 +1,46 @@
+var whatIf = {};
+
+whatIf.init = function(daysInIter, remainingStoryPoints, lastIterEndDate) {
+    alert("whatIf init");
+    try {
+        var dateCalcText = jQuery("#what-if-date"),
+            velocityText = jQuery("#what-if-velocity"),
+            iterationsSpan = jQuery("#what-if-iterations"),
+            debugInfo = jQuery("#debug-info");
+
+        var dateDiffInDays = function(d1, d2) {
+            var t2 = d2.getTime();
+            var t1 = d1.getTime();
+            return parseInt((t2 - t1) / (24 * 3600 * 1000));
+        };
+
+        var remainingIterations = function(velocity, remaining_story_points) {
+            return Math.ceil(remaining_story_points / velocity);
+        };
+
+        var expectedCompletionDateFor = function(lastIterEndDate, daysInIter, remainingIterations) {
+            return new Date(lastIterEndDate.getTime() + (1000 * 60 * 60 * 24 * (daysInIter * remainingIterations)));
+        };
+
+        velocityText.blur(function() {
+            var velocity = parseInt(velocityText.val());
+            var iterations = remainingIterations(velocity, remainingStoryPoints);
+            var expectedDate = expectedCompletionDateFor(lastIterEndDate, daysInIter, iterations);
+            var dateString = expectedDate.getFullYear() + '-' + (expectedDate.getMonth() + 1) + '-' + expectedDate.getDate();
+            iterationsSpan.html(iterations);
+            dateCalcText.val(dateString);
+        });
+
+        dateCalcText.blur(function() {
+            var desiredEndDate = new Date(dateCalcText.val());
+            var dayDiff = dateDiffInDays(lastIterEndDate, desiredEndDate);
+            var iterations = Math.ceil(dayDiff / daysInIter);
+            var requiredVelocity = remainingStoryPoints / iterations;
+            iterationsSpan.html(iterations);
+            velocityText.val(requiredVelocity);
+        });
+
+    } catch(err) {
+        debugInfo.html(err);
+    }
+};
--- release_metrics/lib/what_if_scenario.rb
+++ release_metrics/lib/what_if_scenario.rb
@@ -16,54 +16,15 @@ module CustomMacro
 <span id='debug-info'></span>
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+<script language="javascript" type="text/javascript" src="../../../../plugin_assets/release_metrics/javascripts/what.if.js"></script>
 <script type="text/javascript">
   jQuery.noConflict();
   // register the initialize function for executing after page loaded.
   MingleJavascript.register(function initialize() {
     try {
-
-      var lastIterEndDate = new Date('#{@last_iter_end_date.to_s}'),
-          daysInIter = #{@days_in_iter},
-          remainingStoryPoints = #{@remaining_story_points},
-          dateCalcText = jQuery("#what-if-date"),
-          velocityText = jQuery("#what-if-velocity"),
-          iterationsSpan = jQuery("#what-if-iterations"),
-          debugInfo   = jQuery("#debug-info");
-
-      var dateDiffInDays = function(d1, d2) {
-        var t2 = d2.getTime();
-        var t1 = d1.getTime();
-        return parseInt((t2-t1)/(24*3600*1000));
-      };
-
-      var remainingIterations = function(velocity, remaining_story_points) {
-        return Math.ceil(remaining_story_points / velocity);
-      };
-
-      var expectedCompletionDateFor = function(lastIterEndDate, daysInIter, remainingIterations) {
-        return new Date(lastIterEndDate.getTime() + (1000 * 60 * 60 * 24 * (daysInIter * remainingIterations)));
-      };
-
-      velocityText.blur(function() {
-        var velocity = parseInt(velocityText.val());
-        var iterations = remainingIterations(velocity, remainingStoryPoints);
-        var expectedDate = expectedCompletionDateFor(lastIterEndDate, daysInIter, iterations);
-        var dateString = expectedDate.getFullYear() + '-' + (expectedDate.getMonth()+1) + '-' + expectedDate.getDate();
-        iterationsSpan.html(iterations);
-        dateCalcText.val(dateString);
-      });
-
-      dateCalcText.blur(function() {
-        var desiredEndDate = new Date(dateCalcText.val());
-        var dayDiff = dateDiffInDays(lastIterEndDate, desiredEndDate);
-        var iterations = Math.ceil(dayDiff / daysInIter);
-        var requiredVelocity = remainingStoryPoints / iterations;
-        iterationsSpan.html(iterations);
-        velocityText.val(requiredVelocity);
-      });
-
+      whatIf.init(#{@days_in_iter}, #{@remaining_story_points}, new Date('#{@last_iter_end_date.to_s}'));
     } catch(err) {
-      debug-info.html(err);
+      alert(err);
     }
   });
 </script>
